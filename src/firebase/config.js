e74cd454a6c50f2cceba36d53bfc0ed20ad5f6cf
shogil <shogilbabu@gmail.com>
2021-09-14 15:08:25 +0530

    firebase added

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA3v2X3VvI5kT1mBa8KPObD_xspBXmLPb8",
+    authDomain: "olxabc.firebaseapp.com",
+    projectId: "olxabc",
+    storageBucket: "olxabc.appspot.com",
+    messagingSenderId: "1002389747390",
+    appId: "1:1002389747390:web:6011f974178e9f786b90ef",
+    measurementId: "G-DZ5MKDG3YY"
+  };
