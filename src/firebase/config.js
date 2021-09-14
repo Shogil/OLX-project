5bf3cd5b7e3277d5176df65ea606250ae0c900ff
shogil <shogilbabu@gmail.com>
2021-09-14 19:49:58 +0530

    update the firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/firebase'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyA3v2X3VvI5kT1mBa8KPObD_xspBXmLPb8",
@@ -9,3 +11,5 @@ const firebaseConfig = {
     appId: "1:1002389747390:web:6011f974178e9f786b90ef",
     measurementId: "G-DZ5MKDG3YY"
   };
+
+  export default firebase.initializeApp(firebaseConfig);
